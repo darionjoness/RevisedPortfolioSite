38d75f005bb557ce13ee665e564e2cfe195aa795
darionjoness <101945727+darionjoness@users.noreply.github.com>
2023-03-25 11:43:56 -0500

    changed about paragraph

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -23,7 +23,7 @@ const About = ({ toggleNav }: AboutTypes) => {
             </div>
             <div className="aboutText">
                 <h1>About Me</h1>
-                <p>Hi, my name is Darion and I'm a 21 year old self taught Front end Developer from Stillwater Oklahoma! I love to code! In my free time when I'm not coding, I like to invest in crypto, watch UFC fights, play basketball, play with my dog named bellah and hangout with my family! I work hard and love to learn!</p>
+                <p>Hi, my name is Darion and I'm a 21 year old self taught Front end Developer from Stillwater Oklahoma! I love to code! In my free time when I'm not coding, I like to watch UFC, play basketball, play with my dog named bellah and hangout with my family! I work hard and love to learn!</p>
                 <div className="socialLinks">
                     <a target={'_blank'} href="https://github.com/darionjoness"><AiFillGithub /></a>
                     <a target={'_blank'} href="https://www.linkedin.com/in/darion-jones-52a001227/"><AiFillLinkedin /></a>
